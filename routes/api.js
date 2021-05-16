131e2abd10af4139f90276aaad948f427440f3d0
AK <amashokkumar@hotmail.com>
2021-05-16 19:42:43 +0530

    API Done responses pending

---
+++ routes/api.js
@@ -0,0 +1,154 @@
+var express = require("express");
+var router = express.Router();
+const Joi = require('joi');
+const _ = require('lodash');
+const { addMovie, signUp, addComment } = require('../validator/api.validator')
+const Response = require("../responses/responses");
+const { messages, status } = require("../configs");
+const { fileUpload } = require('../utils/uploads')
+const movieModel = require('../models/movie');
+const movieCommentsModel = require('../models/movieComments');
+const userModel = require('../models/user');
+const { generateHash } = new userModel();
+const authMethods = require('../utils/authMethods');
+const verifyToken = require('../hooks/verifyToken');
+
+/* Post Movie*/
+router.post("/v1/movie/add", fileUpload.single('fileKey'), async (req, res, next) => {
+    try {
+
+        if (req.file) {
+            req.body.moviePhoto = req.file.filename
+        }
+
+        var insertMovie = await movieModel.create(req.body)
+
+        return Response.success(req, res, status.HTTP_OK, insertMovie, "Movie created successfully")
+    }
+    catch (error) {
+        next({ status: status.HTTP_INTERNAL_SERVER_ERROR, message: JSON.stringify({ message: error.message, stack: error.stack }) });
+    }
+});
+
+/* Sign up Section */
+router.post("/v1/user/signup", signUp, async (req, res, next) => {
+    try {
+
+        let checkUser = await userModel.findOne({
+            email: req.body.email
+        })
+
+        if (checkUser) {
+            return Response.errors(req, res, status.HTTP_BAD_REQUEST, "User already exist")
+        }
+
+        req.body.password = generateHash(req.body.password)
+
+        var insertUser = await userModel.create(req.body)
+
+        return Response.success(req, res, status.HTTP_OK, insertUser, "User signup successfully")
+    }
+    catch (error) {
+        next({ status: status.HTTP_INTERNAL_SERVER_ERROR, message: JSON.stringify({ message: error.message, stack: error.stack }) });
+    }
+});
+
+/* Add movie*/
+router.post("/v1/user/login", signUp, async (req, res, next) => {
+    try {
+        let checkUser = await userModel.findOne({
+            email: req.body.email
+        })
+
+        if (!checkUser) {
+            return Response.errors(req, res, status.HTTP_BAD_REQUEST, "User not found")
+        }
+
+        let checkPassword = checkUser.validatePassword(req.body.password)
+        if (!checkPassword) {
+            return Response.errors(req, res, status.HTTP_UNAUTHORIZED, "Password wrong")
+        }
+
+        let generateToken = await authMethods.generateToken({
+            userId: checkUser._id
+        })
+
+        var result = {
+            _id: checkUser._id,
+            email: checkUser.email,
+            token: generateToken
+        }
+
+        console.log("checkPassword", checkPassword);
+        return Response.success(req, res, status.HTTP_OK, result, "User login successfully")
+    }
+    catch (error) {
+        next({ status: status.HTTP_INTERNAL_SERVER_ERROR, message: JSON.stringify({ message: error.message, stack: error.stack }) });
+    }
+});
+
+/* Add movie*/
+router.get("/v1/movie/list", async (req, res, next) => {
+    try {
+        let listMovies = await movieModel.find({
+            status: 1
+        }).lean()
+
+        console.log("listMovies", listMovies);
+        return Response.success(req, res, status.HTTP_OK, listMovies, "Movie listed successfully")
+    }
+    catch (error) {
+        console.log("error", error);
+        next({ status: status.HTTP_INTERNAL_SERVER_ERROR, message: JSON.stringify({ message: error.message, stack: error.stack }) });
+    }
+});
+
+/* Add movie*/
+router.get("/v1/movie/list/:movieSlug", async (req, res, next) => {
+    try {
+        let listMovies = await movieModel.findOne({
+            movieSlug: req.params.movieSlug,
+            status: 1
+        })
+            .populate([{
+                path: 'comments',
+                populate: {
+                    path: 'userId',
+                    select: ('_id', 'name', 'email')
+                }
+            }])
+
+        console.log("listMovies", listMovies);
+        return Response.success(req, res, status.HTTP_OK, listMovies, "Movie listed successfully")
+    }
+    catch (error) {
+        console.log("error", error);
+        next({ status: status.HTTP_INTERNAL_SERVER_ERROR, message: JSON.stringify({ message: error.message, stack: error.stack }) });
+    }
+});
+
+router.post("/v1/movie/add/comment", verifyToken, addComment, async (req, res, next) => {
+    try {
+        console.log("req.user", req.user);
+        var userId = req.user._id
+        var addComment = await movieCommentsModel.create({
+            comment: req.body.comment,
+            userId: userId
+        })
+        await movieModel.updateOne({
+            _id: req.body.movieId
+        },
+            {
+                $push: {
+                    comments: addComment._id
+                }
+            })
+
+        return Response.success(req, res, status.HTTP_OK, null, "Comment added successfully")
+    }
+    catch (error) {
+        next({ status: status.HTTP_INTERNAL_SERVER_ERROR, message: JSON.stringify({ message: error.message, stack: error.stack }) });
+    }
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res,) => {
+  res.render('index', { title: 'Filmy Application' });
+});
+
+// Filmy View 
+router.get('/films', (req, res,) => {
+  res.render('films', { title: 'Film Page - Filmy Application' });
+});
+
+router.get('/login', (req, res,) => {
+  res.render('login', { title: 'Login Page - Filmy Application' });
+});
+
+router.get('/signup', (req, res,) => {
+  res.render('signup', { title: 'Signup Page - Filmy Application' });
+});
+
+router.get('/create', (req, res,) => {
+  res.render('create', { title: 'Movie Creating Page - Filmy Application' });
 });
 
 module.exports = router;
