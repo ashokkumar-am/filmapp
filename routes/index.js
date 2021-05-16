b5310ebb17ccca2444ce21323328755ba9db8ce1
AK <amashokkumar@hotmail.com>
2021-05-17 00:15:09 +0530

    UI Altered Partially

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('Home Page Crawling...')
+  res.render('index', { title: 'Filmy App' });
+  next();
 });
 
 router.get('/films', function (req, res, next) {
-  res.render('films', { title: 'Express' });
+  res.render('films', { title: 'Filmy App' });
 });
 
 router.get('/login', function (req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('login', { title: 'Filmy App' });
 });
 router.get('/signup', function (req, res, next) {
-  res.render('register', { title: 'Express' });
+  res.render('register', { title: 'Filmy App' });
 });
 
 router.get('/create', function (req, res, next) {
-  res.render('create', { title: 'Express' });
+  res.render('create', { title: 'Filmy App' });
 });
+
+// app.use('/verify', function (req, res, next) {
+//   console.log("Authenticate and Redirect")
+//   res.redirect('/user');
+//   next();
+// });
+
+// app.get('/user', function (req, res) {
+//   res.send("User Page");
+// });
 module.exports = router;
