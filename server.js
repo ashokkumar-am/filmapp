068331465f60038d655b842c92b0db5ba7ebecfa
AK <amashokkumar@hotmail.com>
2021-05-17 11:59:22 +0530

    PORT Configured

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const app = require('./app');
 
-const port = process.env.port;
+const port = process.env.port || 4000;
 
 app.listen(port, () => {
     console.log(`Filmy App Server listening at http://localhost:${port}`);
