0604ebe59284ea9237a87adba881b83c030c8a99
AK <amashokkumar@hotmail.com>
2021-05-17 13:17:34 +0530

    PORT bug resolved

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // SERVER.js
 
 const app = require('./app');
+const port = process.env.port || 4000;
 
-
-app.listen(process.env.port || 4000, () => {
+app.listen(port, () => {
     console.log(`Filmy App Server listening at http://localhost:${port}`);
 });
