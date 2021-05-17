cab28d6863ce295e803408212398e24eac4daaf0
AK <amashokkumar@hotmail.com>
2021-05-17 12:11:28 +0530

    SERVER PORT reset

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // SERVER.js
 
 const app = require('./app');
-const port = process.env.port || 4000;
 
-app.listen(port, () => {
+
+app.listen(process.env.port || 4000, () => {
     console.log(`Filmy App Server listening at http://localhost:${port}`);
 });
